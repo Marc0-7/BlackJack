559e8b0c11ed3aa597c9f59130c33ec21b73d12c
Marc0-7 <marcoelyas77@gmail.com>
2025-06-02 13:17:33 +0200

    Update blackjack_script.js

--- blackjack_script.js
+++ blackjack_script.js
@@ -1,43 +1,42 @@
-//header
-function OnLine()
-{
-    alert("prossimamente...")
-}
-//mazzo di carte, Picche = \u2660, Cuori = \u2665, Quadri = \u2666, Fiori = \u2663
-let carte = 
-[
-    {nome: "A_cuori", img: Object.assign(new Image(), {src: "./Blackjack/img/ace_of_hearts"})},
-    {nome: "2_cuori", img: Object.assign(new Image(), {src: ""})},
-    {nome: "3_cuori", img: Object.assign(new Image(), {src: ""})},
-    {nome: "4_cuori", img: Object.assign(new Image(), {src: ""})},
-    {nome: "5_cuori", img: Object.assign(new Image(), {src: ""})},
-    {nome: "6_cuori", img: Object.assign(new Image(), {src: ""})},
-    {nome: "7_cuori", img: Object.assign(new Image(), {src: ""})},
-    {nome: "8_cuori", img: Object.assign(new Image(), {src: ""})},
-    {nome: "9_cuori", img: Object.assign(new Image(), {src: ""})},
-    {nome: "10_cuori", img: Object.assign(new Image(), {src: ""})},
-    {nome: "J_cuori", img: Object.assign(new Image(), {src: ""})},
-    {nome: "Q_cuori", img: Object.assign(new Image(), {src: ""})},
-    {nome: "K_cuori", img: Object.assign(new Image(), {src: ""})},
+<html>
+<head>
+    <!--collegamento file css e js-->
+    <link rel="stylesheet" href="blackjack.css">
+    <script src="blackjack.js" defer></script>
+    <!--meta tag-->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--titolo-->
+    <title>BlackJack</title>
+</head>
+<body>
+    <!--header-->
+    <header>
+        <h1>BlackJack</h1>
+        <div>
+            <button type="button">Bot</button>
+            <button type="button" onclick="OnLine()">On-Line</button>
+        </div>        
+    </header>
+    <!--partita!-->
+    <!--zonagiocatori-->
+    <section id="section1">
+        <div id="player">
+            
+        </div>
+        <div id="bot1">
 
-]
-for (let i = 0; i < carte.length; i++)
-{
+        </div>
+        <div id="bot2">
 
-}
-//.
-//.
-let g1 = [], g2 = [], g3 = [], g4 = [];
-//carta random dal mazzo
-function daiCarte()
-{
-    let dim = (Math.floor(Math.random() * carte.length * carte[0].length)), x;
-    for(let i = 0; i < carte.length; i++)
-        for(let j = 0; j < carte[0].length; j++)
-        {
-            if(dim == 0)
-                return x = carte[i][j];
-            dim--;
-        }
-}
+        </div>
+        <div id="bot3">
 
+        </div>
+        <!--tavolo-->
+        <section id="section2">
+
+        </section>
+    </section>
+</body>
+</html>
